5b95f26e00e9181fbc6a110f29a5642e9b75a06e
Hawdiya <hawdiyaaomer@gmail.com>
2025-12-15 22:28:35 +0300

    week 2

--- week2/inheritanceOverridingInterface.dart
+++ week2/inheritanceOverridingInterface.dart
@@ -22,6 +22,29 @@ class Phone extends Device{
   }
 }
 
+class person{
+  String name;
+  int age;
+
+  person(this.name, this.age);
+
+  void introduce(){
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
+
+class student extends person{
+  String major;
+
+  student(String name, int age, this.major): super(name, age);
+
+  @override
+  void introduce(){
+    super.introduce();
+    print('I am studying $major.');
+  }
+}
+
 void main(){
 
 Device d1= new Laptop();
